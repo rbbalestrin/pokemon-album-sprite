6c6f69b6356c059ad9a58321556043cf3fe8195c
rbbalestrin <therealrenanbez@gmail.com>
2025-03-13 10:08:17 -0300

    Alterado adaptador para suporte à Vercel com Node.js 20

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'nodejs20.x' // Força versão 20
+			// Configuração específica para Vercel
+			runtime: 'nodejs20.x', // Usando Node.js 20 que é suportado pela Vercel
+			split: false // Compile tudo em um único arquivo de função
 		})
 	}
 };
